a49d0b1c9dbb7e5182d79e1eda9cf40aa89dfa96
Emmytobs <eotobo11@gmail.com>
2019-11-29 13:30:54 +0100

    Added barrels to organize the imports in the app.module.ts file and in the routes.ts file.
    Added three index.ts files in the events directory and in the two sub-directories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  CreateEventComponent
+} from './events/index';
+
 import { EventsAppComponent } from './events-app.component';
-import { EventsListComponent } from './events/events-list.component';
-import { EventThumbnailComponent } from './events/event-thumbnail.component';
 import { NavBarComponent } from './nav/nav-bar.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
 import { appRoutes } from './routes';
-import { CreateEventComponent } from './events/create-event.component';
 import { PageNotFoundComponent } from './errors/page-not-found.component';
-import { ProfileComponent } from './user/profile.component';
 
 // import { EventsListResolverService } from './events/events-list-resolver.service';
 
@@ -41,7 +43,7 @@ export class AppModule { }
 // Function for preventing the user from navigating from the unsaved create-event page
 export function checkDirtyState(component: CreateEventComponent) {
   if (component.isDirty) {
-    return window.confirm('ARe you sure you want to cancel? You event data will not be saved');
+    return window.confirm('Are you sure you want to cancel? You event data will not be saved');
   } else {
     return true;
   }
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,18 +1,33 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
 
-import { EventsListComponent } from './events/events-list.component';
-import { EventDetailsComponent } from './events/event-details/event-details.component';
-import { CreateEventComponent } from './events/create-event.component';
-import { PageNotFoundComponent } from './errors/page-not-found.component';
-import { EventDetailsGuard } from './events/event-details/event-details.guard';
-import { EventsListResolverService } from './events/events-list-resolver.service';
+import {
+  EventsListComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventDetailsGuard,
+  EventsListResolverService
+} from "./events/index";
+
+import { PageNotFoundComponent } from "./errors/page-not-found.component";
 
 export const appRoutes: Routes = [
-  {path: 'events/new', canDeactivate: ['canDeactivateCreateEvent'], component: CreateEventComponent},
-  {path: 'events', component: EventsListComponent, resolve: {events: EventsListResolverService}},
-  {path: 'events/:id', canActivate: [EventDetailsGuard], component: EventDetailsComponent},
-  {path: '404', component: PageNotFoundComponent},
-  {path: '', redirectTo: '/events', pathMatch: 'full'},
+  {
+    path: "events/new",
+    canDeactivate: ["canDeactivateCreateEvent"],
+    component: CreateEventComponent
+  },
+  {
+    path: "events",
+    component: EventsListComponent,
+    resolve: { events: EventsListResolverService }
+  },
+  {
+    path: "events/:id",
+    canActivate: [EventDetailsGuard],
+    component: EventDetailsComponent
+  },
+  { path: "404", component: PageNotFoundComponent },
+  { path: "", redirectTo: "/events", pathMatch: "full" },
   // Lazily load the the usermodule when the path starts with '/user'
-  {path: 'user', loadChildren: './user/user.module#UserModule'}
+  { path: "user", loadChildren: "./user/user.module#UserModule" }
 ];
